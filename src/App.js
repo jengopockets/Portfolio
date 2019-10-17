993d2b688d1cea7f4c802706ab6f5ae436cf83c6
Jengopockets <jengopockets@gmail.com>
2019-10-17 15:02:59 -0400

    add project photo's

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     {
     id: 1,
     project_name: 'Crime Stats For Travelers',
-    project_img: 'https://raw.githubusercontent.com/Bryce-Soghigian/Ocean_Portfolio/master/src/Components/Projects/Crime.png',
+    project_img: 'https://raw.githubusercontent.com/jengopockets/Portfolio/master/src/Components/Project/Crime.png',
     project_description:'This was a project where we mapped US crime data from the FBI Database.',
     },
     {
     id: 2,
-    project_name: 'Crime Stats For Travelers',
-    project_img: './Crime.png',
-    project_description:'This was a project where we mapped US crime data from the FBI Database.',
+    project_name: 'Random Act Generator',
+    project_img: 'https://raw.githubusercontent.com/jengopockets/Portfolio/master/src/Components/Project/Random.png',
+    project_description:'I wrote the backend to this project where I allowed for full CRUD fuctionallity and included an endpoint that would randomize data from the database to give to the front-end.',
     },
     {
     id: 3,
