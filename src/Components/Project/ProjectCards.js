9c6a1f593d51e6ad0197e9e35691904f2fd31935
Jengopockets <jengopockets@gmail.com>
2019-10-17 14:35:27 -0400

    add project photo's

--- src/Components/Project/ProjectCards.js
+++ src/Components/Project/ProjectCards.js
@@ -3,7 +3,7 @@ import './Project.css'
 
 const Card = (props) => {
     return(
-        <div key={props.card.id}>
+        <div class="card" key={props.card.id}>
             <h2>{props.card.project_name}</h2>
             <img src={props.card.project_img} alt="Project" />
             <p>{props.card.project_description}</p>  
